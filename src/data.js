1086208e13398fff578cbad59510c9d9af8f1d7c
Fixito <thomas_3004@hotmail.fr>
2021-10-06 09:55:24 +0200

    updated projects

--- src/data.js
+++ src/data.js
@@ -1,66 +1,73 @@
 export const projects = [
   {
-    title: "Pomodoro Clock",
-    subtitle: "React, CSS",
-    description: "Un minutueur basé sur la technique Pomodoro.",
-    image: "./project-7.png",
-    link: "https://github.com/Fixito/pomodoro-clock",
+    title: 'Pomodoro Clock',
+    subtitle: 'React, CSS',
+    description: 'Un minutueur basé sur la technique Pomodoro.',
+    image: './project-7.png',
+    link: 'https://github.com/Fixito/pomodoro-clock'
   },
   {
-    title: "Calculator",
-    subtitle: "React, CSS",
+    title: 'Calculator',
+    subtitle: 'React, CSS',
     description:
-      "Une calculatrice qui permet de faire les opérations arithmétiques de base.",
-    image: "./project-6.png",
-    link: "https://github.com/Fixito/react-calculator",
+      'Une calculatrice qui permet de faire les opérations arithmétiques de base.',
+    image: './project-6.png',
+    link: 'https://github.com/Fixito/react-calculator'
   },
   {
-    title: "Drum Machine",
-    subtitle: "React, CSS",
+    title: 'Drum Machine',
+    subtitle: 'React, CSS',
     description:
-      "Une drum machine qui utilise les click et keyPress events pour jouer les samples.",
-    image: "./project-5.png",
-    link: "https://github.com/Fixito/drum-machine",
+      'Une drum machine qui utilise les click et keyPress events pour jouer les samples.',
+    image: './project-5.png',
+    link: 'https://github.com/Fixito/drum-machine'
   },
   {
-    title: "Todo App",
-    subtitle: "React, Material-UI",
+    title: 'Todo App',
+    subtitle: 'React, Material-UI',
     description:
-      "Todo-list réalisée avec des hooks, des contexts et persistance des données dans le LocalStorage ",
-    image: "./project-1.png",
-    link: "https://github.com/Fixito/todos-hook",
+      'Todo-list réalisée avec des hooks, des contexts et persistance des données dans le LocalStorage ',
+    image: './project-1.png',
+    link: 'https://github.com/Fixito/todos-hook'
   },
   {
-    title: "API Weathter",
-    subtitle: "HTML, CSS, Javascript",
+    title: 'API Weathter',
+    subtitle: 'HTML, CSS, Javascript',
     description:
-      "Le projet manipulant une API publique pour développer une appli météo.",
-    image: "./project-2.png",
-    link: "https://github.com/Fixito/api-weather",
+      'Le projet manipulant une API publique pour développer une appli météo.',
+    image: './project-2.png',
+    link: 'https://github.com/Fixito/api-weather'
   },
   {
-    title: "Blog sous Symfony",
-    subtitle: "Bootstrap, PHP, Symfony",
-    description: "Un blog réalisé dans le cadre de ma formation",
-    image: "./project-3.jpg",
-    link: "https://gitlab.com/Fixito/thomasrobert",
+    title: 'Social App',
+    subtitle: 'MongoDB, Express,React, NodeJS et GraphQl',
+    description: 'Une aplli web de type réseau social.',
+    image: './project-8.png',
+    link: 'https://social-app-merng-tr.netlify.app/'
+  }
+  /*   {
+    title: 'Blog sous Symfony',
+    subtitle: 'Bootstrap, PHP, Symfony',
+    description: 'Un blog réalisé dans le cadre de ma formation',
+    image: './project-3.jpg',
+    link: 'https://gitlab.com/Fixito/thomasrobert'
   },
-  /*  {
-    title: "Color Game",
-    subtitle: "HTML, CSS, Javascript",
-    description: "Un jeu où il faut deviner la couleur selon son code RGB.",
-    image: "./project-4.jpg",
-    link: "https://github.com/Fixito/color-game",
-  }, */
+  {
+    title: 'Color Game',
+    subtitle: 'HTML, CSS, Javascript',
+    description: 'Un jeu où il faut deviner la couleur selon son code RGB.',
+    image: './project-4.jpg',
+    link: 'https://github.com/Fixito/color-game'
+  } */
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "PHP",
-  "Git",
-  "Bootstrap",
-  "Symfony",
-  "Worpress",
+  'JavaScript',
+  'React',
+  'Next.js',
+  'PHP',
+  'Git',
+  'Bootstrap',
+  'Symfony',
+  'Worpress'
 ];
