e99011cf069db2fc3ff54e043934e2dcac08fe1b
Fixito <thomas_3004@hotmail.fr>
2021-10-08 13:16:05 +0200

    added smooth scroll

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,53 @@
-import React from "react";
+import React from 'react';
 
 function About() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const target = e.target.getAttribute('href');
+    const location = document.querySelector(target).offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      top: location - 72
+    });
+  };
+
   return (
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+    <section id='about'>
+      <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
+        <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+          <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
             Salut, je suis Thomas.&nbsp;
-            <br className="hidden lg:inline-block" />
+            <br className='hidden lg:inline-block' />
             J'aime construire de superbes applications
           </h1>
-          <p className="mb-8 leading-relaxed">
+          <p className='mb-8 leading-relaxed'>
             Depuis que j'ai commencé mon parcours de développeur web en 2020, je
             cherche à collaborer avec des entreprises et des personnes
             talentueuses pour créer des produits numériques destinés à la fois
             aux entreprises et aux consommateurs. Je suis naturellement curieux
             et je travaille constamment à améliorer mes compétences.
           </p>
-          <div className="flex justify-center">
+          <div className='flex justify-center'>
             <a
-              href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
-            >
+              href='#contact'
+              className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'
+              onClick={handleClick}>
               Travailler avec moi
             </a>
             <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
-            >
+              href='#projects'
+              className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'
+              onClick={handleClick}>
               Voir mes projets
             </a>
           </div>
         </div>
-        <div className="lg:max-w-md lg:w-full md:w-1/2 w-5/6">
+        <div className='lg:max-w-md lg:w-full md:w-1/2 w-5/6'>
           <img
-            className="object*cover object-center rounded"
-            src="./thomas.jpg"
-            alt="hero"
+            className='object*cover object-center rounded'
+            src='./thomas.jpg'
+            alt='hero'
           />
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,65 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import React from "react";
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import React from 'react';
 
 function Navbar() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const target = e.target.getAttribute('href');
+    const location = document.querySelector(target).offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      top: location - 72
+    });
+  };
+
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-20">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <div className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+    <header className='bg-gray-800 md:sticky top-0 z-20'>
+      <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+        <div className='title-font font-medium text-white mb-4 md:mb-0'>
+          <a href='#about' className='ml-3 text-xl'>
             Thomas ROBERT
           </a>
         </div>
-        <nav className="md:mr-auto md:ml-4 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+        <nav className='md:mr-auto md:ml-4 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center'>
+          <a
+            href='#projects'
+            className='mr-5 hover:text-white'
+            onClick={handleClick}>
             Mes projets
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a
+            href='#skills'
+            className='mr-5 hover:text-white'
+            onClick={handleClick}>
             Mes compétences
           </a>
-          <a href="./CV.pdf" className="mr-5 hover:text-white" download>
+          <a href='./CV.pdf' className='mr-5 hover:text-white' download>
             CV
           </a>
         </nav>
-        <div className="inline-flex items-center">
+        <div className='inline-flex items-center'>
           <a
-            href="https://github.com/Fixito"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-800 border-0 py-1 px-2 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-          >
-            <AiFillGithub className="w-5 h-5 ml-1" />
+            href='https://github.com/Fixito'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='bg-gray-800 border-0 py-1 px-2 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0'>
+            <AiFillGithub className='w-5 h-5 ml-1' />
           </a>
           <a
-            href="https://www.linkedin.com/in/thomasrobert1/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-800 border-0 py-1 px-2 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-          >
-            <AiFillLinkedin className="w-5 h-5 ml-1" />
+            href='https://www.linkedin.com/in/thomasrobert1/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='bg-gray-800 border-0 py-1 px-2 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0'>
+            <AiFillLinkedin className='w-5 h-5 ml-1' />
           </a>
           <a
-            href="#contact"
-            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-          >
+            href='#contact'
+            className='inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline hover:bg-gray-700 rounded text-base mt-4 md:mt-0'
+            onClick={handleClick}>
             Me contacter
-            <ArrowRightIcon className="w-4 h-4 ml-1" />
+            <ArrowRightIcon className='w-4 h-4 ml-1' />
           </a>
         </div>
       </div>
